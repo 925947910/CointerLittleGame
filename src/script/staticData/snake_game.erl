45ea46ebb6f86d49fca8c931639e3f49a57f8291
yeah <925947910@qq.com>
2020-01-09 20:07:43 +0800

    Signed-off-by: yeah <925947910@qq.com>

--- src/script/staticData/snake_game.erl
+++ src/script/staticData/snake_game.erl
@@ -9,8 +9,8 @@
 %% ====================================================================
 -export([get_data/1,get_ids/0]).
 get_data(1)->#snake_game{id=1,coin=100,maxPlayer=15,minPlayer=2,totalScore=5000};
-get_data(2)->#snake_game{id=2,coin=500,maxPlayer=15,minPlayer=2,totalScore=5000};
-get_data(3)->#snake_game{id=3,coin=1000,maxPlayer=15,minPlayer=2,totalScore=5000};
+get_data(2)->#snake_game{id=2,coin=1000,maxPlayer=15,minPlayer=2,totalScore=5000};
+get_data(3)->#snake_game{id=3,coin=5000,maxPlayer=15,minPlayer=2,totalScore=5000};
 get_data(4)->#snake_game{id=4,coin=10000,maxPlayer=15,minPlayer=2,totalScore=5000};
 get_data(5)->?UNDEFINED.
 
